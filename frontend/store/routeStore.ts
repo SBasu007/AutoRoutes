ec2d1d873453393229cbe4af841e44faaf2c1e48
SBasu007 <soumyadeep.basu19@gmail.com>
2026-06-12 17:31:46 +0530

    Changed frontend config files 2

--- frontend/store/routeStore.ts
+++ frontend/store/routeStore.ts
@@ -6,11 +6,13 @@ import { Station } from "@/types/station";
 interface RouteStore {
     source: Station | null;
     destination: Station | null;
+    currentRoute: any | null;
     allRoutes: any[];
     selectedRoute: any | null;
 
     setSource: (station: Station | null) => void;
     setDestination: (station: Station | null) => void;
+    setRoute: (route: any | null) => void;
     setAllRoutes: (routes: any[]) => void;
     setSelectedRoute: (route: any | null) => void;
 }
@@ -18,11 +20,13 @@ interface RouteStore {
 export const useRouteStore = create<RouteStore>((set) => ({
     source: null,
     destination: null,
+    currentRoute: null,
     allRoutes: [],
     selectedRoute: null,
 
     setSource: (station) => set({ source: station }),
     setDestination: (station) => set({ destination: station }),
+    setRoute: (route) => set({ currentRoute: route }),
     setAllRoutes: (routes) => set({ allRoutes: routes }),
     setSelectedRoute: (route) => set({ selectedRoute: route }),
 }));
